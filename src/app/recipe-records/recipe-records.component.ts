215e90e8bc8c8eb767a2d8c6a4e55b2199e8bf81
Mavya Soni <mavya.soni@solutionanalysts.com>
2020-01-27 18:41:19 +0530

    [feature] recipe service added

--- src/app/recipe-records/recipe-records.component.ts
+++ src/app/recipe-records/recipe-records.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-records',
-  templateUrl: './recipe-records.component.html'
+  templateUrl: './recipe-records.component.html',
+  providers: [RecipeService]
 })
 export class RecipeRecordsComponent implements OnInit {
 
   @Input() recipes=[];
-  constructor() { }
+  constructor(public mRecipeService: RecipeService){
+    this.recipes = mRecipeService.recipeList
+  }
 
   ngOnInit() {
+
   }
 
 }
